3d29e48f1bdd40170b44bee284455fc8a7238e20
SSvitlana82 <144372279+SSvitlana82@users.noreply.github.com>
2024-05-31 12:08:16 +0200

    modul2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
-// src/index.js
+import { startServer } from './server.js';
+import { initMongoDB } from './db/initMongoDB.js';
 
-import express from 'express';
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
 
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  const data = { message: 'Hello world!' };
-  res.json(data);
-});
-app.use('/numbers', (req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  const err = new Error('помилка');
-  next(err);
-});
-
-app.get('/numbers', (req, res) => {
-  const data = Math.round(Math.random() * 100);
-  res.json(data);
-});
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
